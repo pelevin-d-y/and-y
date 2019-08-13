030a8f6bf60aad029eafb38e5ee44da9d7edc788
pelevin-d-y <natpuot1992@gmail.com>
2019-08-13 17:05:04 +0300

    added oval

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,18 @@ var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 
 $(document).ready(() => {
-  console.log($('body'))
+  const activeOval = $('.club-oval')
+  const items = $('.lavel-item')
+
+  const positionActiveOval = activeOval.position()
+  setTimeout(() => {
+    items.each((index, el) => {
+      const offset = 26
+      const topPosition = $(el).position().top + offset
+
+      $(el).hover(() => {
+        activeOval.css('top', topPosition)
+      })
+    })
+  }, 200)
 })
