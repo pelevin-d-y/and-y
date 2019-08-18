74b2d1745b0007fa475e9e8dfaa5d2d40db6f9be
pelevin-d-y <natpuot1992@gmail.com>
2019-08-18 15:55:58 +0300

    close container

--- src/js/index.js
+++ src/js/index.js
@@ -2,34 +2,37 @@ var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 
 $(document).ready(() => {
-  const activeOval = $('.club-oval')
-  const items = $('.lavel-item')
-
-  const itemHoverHandler = () => {
-    const windowWidth = $( window ).width()
-    const offset = windowWidth <= 1024 ? 36 : 26
-
-    items.each((index, el) => {
-      const topPosition = $(el).position().top + offset
-      
-      $(el).mouseenter(() => {
-        activeOval.css('top', topPosition)
+  if ($('.club-wrapper').length) {
+    const activeOval = $('.club-oval')
+    const items = $('.lavel-item')
+  
+    const itemHoverHandler = () => {
+      const windowWidth = $( window ).width()
+      const offset = windowWidth <= 1024 ? 36 : 26
+  
+      items.each((index, el) => {
+        const topPosition = $(el).position().top + offset
+        
+        $(el).mouseenter(() => {
+          activeOval.css('top', topPosition)
+        })
       })
+    }
+    console.log('aaaaaa')
+    const itemHoverRemoveHandler = () => {
+      items.each((index, el) => {
+        $(el).unbind( "mouseenter" );
+      })
+    }
+  
+    setTimeout(() => {
+      itemHoverHandler()
+    }, 200)
+  
+    $(window).resize(() => {
+      itemHoverRemoveHandler()
+      itemHoverHandler()
     })
+
   }
-
-  const itemHoverRemoveHandler = () => {
-    items.each((index, el) => {
-      $(el).unbind( "mouseenter" );
-    })
-  }
-
-  setTimeout(() => {
-    itemHoverHandler()
-  }, 200)
-
-  $(window).resize(() => {
-    itemHoverRemoveHandler()
-    itemHoverHandler()
-  })
 })
