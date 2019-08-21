de2b01bfde4f4b067d043b1359b610474efb5698
Evgeny Taktarov <je.mouth@gmail.com>
2019-08-21 17:34:27 +0300

    oval

--- src/js/index.js
+++ src/js/index.js
@@ -37,40 +37,6 @@
 //   }
 // })
 
-<<<<<<< Updated upstream
-$(document).ready(() => {
-  if ($('.club-wrapper').length) {
-    const activeOval = $('.club-oval')
-    const items = $('.lavel-item')
-  
-    const itemHoverHandler = () => {
-      const windowWidth = $( window ).width()
-      const offset = windowWidth <= 1024 ? 36 : 26
-  
-      items.each((index, el) => {
-        const topPosition = $(el).position().top + offset
-        
-        $(el).mouseenter(() => {
-          activeOval.css('top', topPosition)
-        })
-      })
-    }
-    console.log('aaaaaa')
-    const itemHoverRemoveHandler = () => {
-      items.each((index, el) => {
-        $(el).unbind( "mouseenter" );
-      })
-    }
-  
-    setTimeout(() => {
-      itemHoverHandler()
-    }, 200)
-  
-    $(window).resize(() => {
-      itemHoverRemoveHandler()
-      itemHoverHandler()
-    })
-=======
 function offset(el) {
   var rect = el.getBoundingClientRect(),
   scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
@@ -98,7 +64,6 @@ function prepAnim(){
 }
 
 prepAnim();
->>>>>>> Stashed changes
 
 //document.addEventListener('DOMContentLoaded', prepAnim);
 window.addEventListener('resize',prepAnim);
